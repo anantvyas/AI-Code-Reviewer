4a94ca0feb834514671bd0bd79f09d04fe3c3ae2
Anant <Vyas.anant2000@gmail.com>
2025-07-18 23:41:51 +0530

    Updated backend logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://ai-code-reviewer-xi-snowy.vercel.app/",
+  credentials: true
+}));
 
 
 app.use(express.json())
